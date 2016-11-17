adc2fc14eb632e782ed549399cba59e614e8b729
abc34 <mail4p@ya.ru>
2016-11-17 15:50:53 +0400

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CENTVER="7"
+NGINX="nginx-1.11.6"
+
+echo "Stopping rockstor service ..."
+service rockstor stop
+
+echo "Update ..."
+rpm -Uvh --force ./$NGINX-1.el$CENTVER.ngx.x86_64.rpm
+echo "Start rockstor service ..."
+service rockstor start
+
